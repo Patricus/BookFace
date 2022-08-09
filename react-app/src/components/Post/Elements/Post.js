df1e00afc756cc7b900be1a607a6ffb67a94e7b6
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-08-08 23:23:50 -0700

    post date and display order

--- react-app/src/components/Post/Elements/Post.js
+++ react-app/src/components/Post/Elements/Post.js
@@ -5,15 +5,34 @@ import Comment from "../../Comment/Elements/Comment";
 import CreateCommentForm from "../../Comment/Form/CreateCommentForm";
 import PostDropdown from "./PostDropdown";
 import "./post.css";
+import PosterInfo from "./PosterInfo";
 
 function Post({ post }) {
     const dispatch = useDispatch();
 
     const user = useSelector(state => state.session.user);
+    const friends = useSelector(state => state.friends);
 
     useEffect(() => {
         if (post) dispatch(getComments(post.id));
     }, [dispatch, post]);
+
+    const post_time = date => {
+        // seconds
+        let post_date = Math.abs(new Date() - new Date(date)) / 1000;
+        if (post_date < 60) return Math.floor(post_date) + "s";
+        // minutes
+        post_date /= 60;
+        if (post_date < 60) return Math.floor(post_date) + "m";
+        // hours
+        post_date /= 60;
+        if (post_date < 24) return Math.floor(post_date) + "h";
+        // days
+        post_date /= 24;
+        if (post_date < 7) return Math.floor(post_date) + "d";
+        // more than a week
+        return new Date(date).toLocaleDateString();
+    };
 
     return (
         <>
@@ -21,6 +40,17 @@ function Post({ post }) {
                 <div className="post-container">
                     <div className="post">
                         {post.user_id === user.id && <PostDropdown post={post} />}
+                        <div>
+                            {post.user_id === user.id ? (
+                                <PosterInfo poster={user} />
+                            ) : (
+                                <PosterInfo poster={friends[post.user_id]} />
+                            )}
+                        </div>
+                        <small>
+                            {post_time(post.edited_at)}
+                            {post.created_at !== post.edited_at && `edited`}
+                        </small>
                         {post.image_link && <img src={post.image_link} alt="post" />}
                         <p>{post.text}</p>
                         <CreateCommentForm post_id={post.id} />
---
+++ react-app/src/components/Post/Elements/PosterInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import defaultProfilePic from "../../images/default-profile.png";
+
+function PosterInfo({ poster }) {
+    return (
+        <div className="poster-info">
+            <img src={poster.profile_pic ? poster.profile_pic : defaultProfilePic} alt="profile" />
+            <h3>{`${poster.first_name} ${poster.last_name}`}</h3>
+        </div>
+    );
+}
+
+export default PosterInfo;
