9e9d41b8b4861d09a88d1275f711441e323bf04f
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-08-10 16:12:09 -0700

    basic contact page

--- react-app/src/components/Contact/pages/ContactPage.js
+++ react-app/src/components/Contact/pages/ContactPage.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "./contactPage.css";
+import githubLogo from "../../images/GitHub_Logo.png";
+import linkedinLogo from "../../images/LI-Logo.png";
 
 function ContactPage() {
     return (
-        <div>
-            <h1>ContactPage</h1>
+        <div id="contact-page">
+            <div className="contact-tile">
+                <strong>By</strong>
+                <h1>Patrick James McPherson</h1>
+            </div>
+                <div className="contact-tile">
+            <a href="https://github.com/Patricus" target="_blank" rel="noreferrer">
+                    <img src={githubLogo} alt="git hub" />
+            </a>
+                </div>
+                <div className="contact-tile">
+            <a
+                href="https://www.linkedin.com/in/patrick-mcpherson-438385117/"
+                target="_blank"
+                rel="noreferrer">
+                    <img src={linkedinLogo} alt="linked in" />
+            </a>
+                </div>
         </div>
     );
 }
