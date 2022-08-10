3362afcdb9ac86d2ff4251d6b7cb087dfdbfaad7
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-08-10 10:38:34 -0700

    right sidebar

--- react-app/src/components/Home/Elements/RightSideBar.js
+++ react-app/src/components/Home/Elements/RightSideBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import "./rightSideBar.css";
 import defaultProfilePic from "../../images/default-profile.png";
 import { getFriends } from "../../../store/friends";
@@ -16,19 +17,25 @@ function RightSideBar() {
     return (
         <div id="right-sidebar">
             <h3>Contacts</h3>
-            {friends &&
+            {friends ? (
                 friends.map(friend => {
-                    {
-                        console.log("friend", friend);
-                    }
-                    <div key={friend.id} className="rightside-friend">
-                        <img
-                            src={friend.profile_pic ? friend.profile_pic : defaultProfilePic}
-                            alt="profile"
-                        />
-                        <span>{`${friend.first_name} ${friend.last_name}`}</span>
-                    </div>;
-                })}
+                    return (
+                        <Link key={friend.id} to={`/profile/${friend.id}/`}>
+                            <div className="rightside-friend">
+                                <img
+                                    src={
+                                        friend.profile_pic ? friend.profile_pic : defaultProfilePic
+                                    }
+                                    alt="profile"
+                                />
+                                <span>{`${friend.first_name} ${friend.last_name}`}</span>
+                            </div>
+                        </Link>
+                    );
+                })
+            ) : (
+                <h4>No friends!</h4>
+            )}
         </div>
     );
 }
