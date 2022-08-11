f613823d330eb1c7121e848bc95d60376e75896b
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-08-11 12:08:55 -0700

    poster check

--- react-app/src/components/Post/Elements/PosterInfo.js
+++ react-app/src/components/Post/Elements/PosterInfo.js
@@ -3,10 +3,17 @@ import defaultProfilePic from "../../images/default-profile.png";
 
 function PosterInfo({ poster }) {
     return (
-        <div className="poster-info">
-            <img src={poster.profile_pic ? poster.profile_pic : defaultProfilePic} alt="profile" />
-            <h3>{`${poster.first_name} ${poster.last_name}`}</h3>
-        </div>
+        <>
+            {poster && (
+                <div className="poster-info">
+                    <img
+                        src={poster.profile_pic ? poster.profile_pic : defaultProfilePic}
+                        alt="profile"
+                    />
+                    <h3>{`${poster.first_name} ${poster.last_name}`}</h3>
+                </div>
+            )}
+        </>
     );
 }
 
