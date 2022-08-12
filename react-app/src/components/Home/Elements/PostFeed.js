79e6bfb6880309b82bd667a556ea2510df51cb15
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-08-12 09:08:20 -0700

    posts friend filter

--- react-app/src/components/Home/Elements/PostFeed.js
+++ react-app/src/components/Home/Elements/PostFeed.js
@@ -7,6 +7,8 @@ import { getFriends } from "../../../store/friends";
 
 function PostFeed() {
     const posts = useSelector(state => state.posts);
+    const friends = useSelector(state => state.friends);
+    const user = useSelector(state => state.session.user);
 
     const dispatch = useDispatch();
 
@@ -20,6 +22,12 @@ function PostFeed() {
             <CreatePostForm />
             {posts &&
                 Object.values(posts)
+                    .filter(post => {
+                        return (
+                            Object.keys(friends).includes(String(post.user_id)) ||
+                            post.user_id === user.id
+                        );
+                    })
                     .sort((a, b) => {
                         return new Date(b.created_at) - new Date(a.created_at);
                     })
