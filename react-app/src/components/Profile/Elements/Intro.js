7ad9f930cb3ec2cea859c2dedba224fac693d5e1
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-08-29 14:06:17 -0700

    fix bio render

--- react-app/src/components/Profile/Elements/Intro.js
+++ react-app/src/components/Profile/Elements/Intro.js
@@ -9,8 +9,12 @@ function Intro({ profile }) {
                 Bio <br />
                 {profile.bio ? profile.bio : `No bio, yet.`}
             </p>
-            {profile.lives_in && <p>{`Lives in ${profile.lives_in}`}</p>}
-            {profile.born_from && <p>{`From ${profile.born_from}`}</p>}
+            {profile.lives_in && profile.lives_in !== ", " && (
+                <p>{`Lives in ${profile.lives_in}`}</p>
+            )}
+            {profile.born_from && profile.born_from !== ", " && (
+                <p>{`From ${profile.born_from}`}</p>
+            )}
         </div>
     );
 }
