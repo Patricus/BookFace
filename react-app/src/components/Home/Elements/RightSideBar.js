ab4d217adb2c626607ad99c68bf38fe62469a765
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-09-16 15:57:39 -0700

    ad on right column

--- react-app/src/components/Home/Elements/RightSideBar.js
+++ react-app/src/components/Home/Elements/RightSideBar.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./rightSideBar.css";
 import defaultProfilePic from "../../images/default-profile.png";
+import patrick from "../../images/patrick.png";
 import { getFriends } from "../../../store/friends";
 
 function RightSideBar() {
@@ -16,6 +17,20 @@ function RightSideBar() {
 
     return (
         <div id="right-sidebar">
+            <h3>Looking for a developer?</h3>
+            <div className="ad">
+                <img src={patrick} alt="Patrick" />
+                <span>
+                    <h4>Patrick McPherson</h4>
+                    <a
+                        href="https://patrickmcpherson.codes/"
+                        target="_blank"
+                        without
+                        rel="noreferrer">
+                        View Portfolio
+                    </a>
+                </span>
+            </div>
             <h3>Contacts</h3>
             {friends ? (
                 friends.map(friend => {
