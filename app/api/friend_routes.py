2ee2b1c2ca529e63e9e6bd27a9ee55f8f69aeccc
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-05-30 12:53:29 -0700

    Refactor friends

--- app/api/friend_routes.py
+++ app/api/friend_routes.py
@@ -52,8 +52,8 @@ def read_friends():
     """
     Read all friends.
     """
-    friends = User.query.join(Friend, and_(Friend.accepted == True, or_(
-        Friend.friend_id == User.id, Friend.user_id == User.id))).filter(User.id == current_user.id).all()
+    friends = User.query.join(Friend, and_(Friend.accepted == True)).filter(
+        Friend.friend_id == User.id or Friend.user_id == User.id and User.id != current_user.id).all()
 
     return {'friends': [friend.to_dict() for friend in friends]}
 
--- react-app/src/components/Home/Elements/RightSideBar.js
+++ react-app/src/components/Home/Elements/RightSideBar.js
@@ -7,52 +7,58 @@ import patrick from "../../images/patrick.png";
 import { getFriends } from "../../../store/friends";
 
 function RightSideBar() {
-    const friends = Object.values(useSelector(state => state.friends));
+  const user = useSelector(state => state.session.user);
+  const friends = Object.values(useSelector(state => state.friends));
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getFriends());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getFriends());
+  }, [dispatch]);
 
-    return (
-        <div id="right-sidebar">
-            <h3>Looking for a developer?</h3>
-            <div className="ad">
-                <img src={patrick} alt="Patrick" />
-                <span>
-                    <h4>Patrick McPherson</h4>
-                    <a
-                        href="https://patrickmcpherson.codes/"
-                        target="_blank"
-                        without
-                        rel="noreferrer">
-                        View Portfolio
-                    </a>
-                </span>
-            </div>
-            <h3>Contacts</h3>
-            {friends ? (
-                friends.map(friend => {
-                    return (
-                        <Link key={friend.id} to={`/profile/${friend.id}/`}>
-                            <div className="rightside-friend">
-                                <img
-                                    src={
-                                        friend.profile_pic ? friend.profile_pic : defaultProfilePic
-                                    }
-                                    alt="profile"
-                                />
-                                <span>{`${friend.first_name} ${friend.last_name}`}</span>
-                            </div>
-                        </Link>
-                    );
-                })
-            ) : (
-                <h4>No friends!</h4>
-            )}
-        </div>
-    );
+  return (
+    <div id="right-sidebar">
+      <h3>Looking for a developer?</h3>
+      <div className="ad">
+        <img
+          src={patrick}
+          alt="Patrick"
+        />
+        <span>
+          <h4>Patrick McPherson</h4>
+          <a
+            href="https://patrickmcpherson.codes/"
+            target="_blank"
+            without
+            rel="noreferrer"
+          >
+            View Portfolio
+          </a>
+        </span>
+      </div>
+      <h3>Contacts</h3>
+      {friends ? (
+        friends.map(friend => {
+          return (
+            <Link
+              key={friend.id}
+              to={`/profile/${friend.id}/`}
+            >
+              <div className="rightside-friend">
+                <img
+                  src={friend.profile_pic ? friend.profile_pic : defaultProfilePic}
+                  alt="profile"
+                />
+                <span>{`${friend.first_name} ${friend.last_name}`}</span>
+              </div>
+            </Link>
+          );
+        })
+      ) : (
+        <h4>No friends!</h4>
+      )}
+    </div>
+  );
 }
 
 export default RightSideBar;
